35cc06b8fba405e9db6e0d18e20309d234c521c4
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-09-29 03:47:57 +0600

    add-a-break-time-added

--- src/components/ExerciseList/ExerciseList.js
+++ src/components/ExerciseList/ExerciseList.js
@@ -12,27 +12,27 @@ const ExerciseList = (props) => {
         totalTime = totalTime + exercise.Time;
     }
 
-    // const [breaks, setBreak] = useState(0);
-    // const breakTime = (breaks) => {
-    //     const newBreak = breaks;
-    //     setBreak(newBreak)
+    const [breaks, setBreak] = useState(0);
+    const breakTime = (breaks) => {
+        const newBreak = breaks;
+        setBreak(newBreak)
 
-    // }
+    }
 
     return (
         <div className='exercise-list-container'>
             <p>selected: {list.length}</p>
             <h4>Add A Break</h4>
-            {/* <button onClick={() => breakTime(10)}>10s</button>
+            <button onClick={() => breakTime(10)}>10s</button>
             <button onClick={() => breakTime(20)}>20s</button>
             <button onClick={() => breakTime(30)}>30s</button>
             <button onClick={() => breakTime(40)}>40s</button>
-            <button onClick={() => breakTime(50)}>50s</button> */}
+            <button onClick={() => breakTime(50)}>50s</button>
 
 
             <h4>Exercise Details</h4>
             <p>Exercise Time: {totalTime}</p>
-            <p>Break Time: </p>
+            <p>Break Time: {breaks}</p>
         </div>
     );
 };
