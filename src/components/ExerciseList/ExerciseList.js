965fbdaf4a66fc7bff72db71d9ab22331d092360
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-09-29 07:36:02 +0600

    profile-added

--- src/components/ExerciseList/ExerciseList.js
+++ src/components/ExerciseList/ExerciseList.js
@@ -1,12 +1,12 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { addToDb } from '../../utilities/fakedb';
-import img from'../../images/img.jpg'
+import img from '../../images/img.jpg'
 import './ExerciseList.css'
 const ExerciseList = (props) => {
-    const {list} = props;
+    const { list } = props;
     console.log(list);
     let totalTime = 0;
-    for(const exercise of list){
+    for (const exercise of list) {
         totalTime = totalTime + exercise.Time;
     }
 
@@ -21,19 +21,27 @@ const ExerciseList = (props) => {
     return (
         <div className='exercise-list-container'>
 
-            <img className='img' src= {img} alt="" />
-            <p className='list-name'>Name: Mujahidul Amin</p>
-            <h4>Add A Break</h4>
-            <button onClick={() => breakTime(10)}>10s</button>
-            <button onClick={() => breakTime(20)}>20s</button>
-            <button onClick={() => breakTime(30)}>30s</button>
-            <button onClick={() => breakTime(40)}>40s</button>
-            <button onClick={() => breakTime(50)}>50s</button>
+            <img className='img' src={img} alt="" />
+            <p className='name'><span className='list-name fw-bolder'>Name:</span> Mujahidul Amin</p>
+            <span className='ms-5 p-4'>Dhaka, Bangladesh</span>
+            <div className='mx-3 p-3 bg-light rounded-3 '>
+                <span className=''>70<span>kg</span></span>
+            </div>
+            <h6 className='mt-2 ms-4'>Add A Break</h6>
+            <div>
+                <div className='mx-3 p-3 bg-light rounded-3 break'>
+                    <button onClick={() => breakTime(10)}>10s</button>
+                    <button onClick={() => breakTime(20)}>20s</button>
+                    <button onClick={() => breakTime(30)}>30s</button>
+                    <button onClick={() => breakTime(40)}>40s</button>
+                    <button onClick={() => breakTime(50)}>50s</button>
+                </div>
+            </div>
 
 
             <h4>Exercise Details</h4>
-            <p>Exercise Time: {totalTime}</p>
-            <p>Break Time: {storedList} Seconds</p>
+            <p className='mx-3 p-3 bg-light rounded-3'>Exercise Time: {totalTime}</p>
+            <p className='mx-3 p-3 bg-light rounded-3'>Break Time: {storedList} Seconds</p>
         </div>
     );
 };
